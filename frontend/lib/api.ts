aed13b5dfced0098de768c6fb3be63d91cd1a874
Tejas2124 <94986801+Tejas2124@users.noreply.github.com>
2025-12-03 12:34:12 +0530

    successfully created authentication service (#35)
    - added handlesubmit for handling signup submit logic
    - updated `main.py` for clean code , using router

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,5 +1,19 @@
 import axios from 'axios';
+export interface RegisterPayload {
+    email :string;
+    password : string;
+}
+
+export interface RegisterResponse {
+    id:string;
+    email:string;
+    is_active:boolean;
+    is_verified:boolean;
+    is_superuser:boolean;
+}
 const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000";
+
+
 export async function loginUser(username: string, password: string) {
     try {
         const formData = new URLSearchParams();
@@ -10,7 +24,7 @@ export async function loginUser(username: string, password: string) {
         formData.append('client_id', '');
         formData.append('client_secret', '');
         const response = await axios.post(
-            `${API_URL}/auth/auth/jwt/login`,
+            `${API_URL}/auth/jwt/login`,
             formData,
             {
                 headers: {
@@ -31,4 +45,31 @@ export async function loginUser(username: string, password: string) {
         console.log("Error Occured while loginUser");
         throw error;
     }
+}
+
+
+
+
+
+// change in the production #prod.
+
+export async function handleSignup(payload:RegisterPayload):Promise<RegisterResponse> {
+        
+    
+        const response = await fetch (`${API_URL}/auth/register`,{
+            method:"POST",
+            headers :{
+                'Content-Type':'application/json',
+            },
+            body:JSON.stringify(payload),
+        });
+
+        const data = await response.json()
+
+
+        if (!response.ok){
+            throw new Error(data.detail || "Registration failed");
+        }
+
+        return data as RegisterResponse
 }
