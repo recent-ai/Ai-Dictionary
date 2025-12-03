7a50a875e423ce0595fd73deb55f478132950e1f
TejasR002 <tejas.raval@simformsolutions.com>
2025-12-03 15:19:51 +0530

    fixed route from signup to singin and viseversa

--- frontend/components/login-form.tsx
+++ frontend/components/login-form.tsx
@@ -110,7 +110,7 @@ export function LoginForm({
               <Field>
                 <Button type="submit">Login</Button>
                 <FieldDescription className="text-center">
-                  Don&apos;t have an account? <a href="#">Sign up</a>
+                  Don&apos;t have an account? <a href="/signup">Sign up</a>
                 </FieldDescription>
               </Field>
             </FieldGroup>
