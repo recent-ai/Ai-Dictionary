49bdb17c6211a5d82c5215d8f661843a232fa4f2
Tejas2124 <2x3osjs@gmail.com>
2026-01-05 15:34:44 +0530

    fixed the issue with pdf_parser and arxiv tool with new architecture, also cleaned the diescription workflow file

--- langgraph_bot/workflow/description_workflow.py
+++ langgraph_bot/workflow/description_workflow.py
@@ -27,35 +27,5 @@ desc.add_edge("description_agent",END)
 g = desc.compile()
 
 
-
-inputs = {
-    "messages": [],
-}
-
-final_state =g.invoke(inputs)
-
-
-for msg in final_state:
-    print("------------------------------------------")
-    print(msg)
-    print("------------------------------------------")
-    if msg == "arxiv_urls":
-        print(final_state[msg][0]["abstract"])
-        print()
-    if msg == "tavily_search_result":
-      i = 0
-      for result in final_state[msg]['results']:
-        print(f"result {i}")
-        i+=1
-        print(result['content'])
-        # print(result['url'])
-        print()
-    if msg == "description":
-      print("==================================================================================================================================")
-      print(final_state[msg])
-      print("==================================================================================================================================")
-      print()
-
-
-
 g.get_graph().draw_mermaid_png(output_file_path='./agent_flow_diagrams/description.png')
+
