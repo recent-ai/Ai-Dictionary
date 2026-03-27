fae62e4fb633189c0336a8d680b4de8deb2655dc
HydrallHarsh <harshsantwani.cse21@aii.ac.in>
2026-03-27 09:03:02 +0530

    Update description_workflow.py to skip arxiv_node due to API issues

--- langgraph_bot/workflow/description_workflow.py
+++ langgraph_bot/workflow/description_workflow.py
@@ -18,14 +18,14 @@ from langgraph_bot.tools.tools import title_tool
 desc = StateGraph(state_schema=State)
 # desc.set_entry_point(START)
 desc.add_node("description_agent", description_agent_node)
-desc.add_node("arxiv_node", arxiv_node)
+# desc.add_node("arxiv_node", arxiv_node) # Need to remove this right now due to arxiv api "Rate Exceeded" error (Not our error but arxiv's). We can add it back once the issue is resolved.
 desc.add_node("parser_tool", pdf_parsing_node)
 desc.add_node("tavily_tool", tavily_node)
 
 
 # desc.set_entry_point(START)
-desc.add_edge(START, "arxiv_node")
-desc.add_edge("arxiv_node", "parser_tool")
+desc.add_edge(START, "parser_tool")  # directly to parser tool, skipping arxiv for now.
+# desc.add_edge("arxiv_node", "parser_tool")
 desc.add_edge("parser_tool", "tavily_tool")
 desc.add_edge("tavily_tool", "description_agent")
 # desc.add_edge("parser_tool", "description_agent")
